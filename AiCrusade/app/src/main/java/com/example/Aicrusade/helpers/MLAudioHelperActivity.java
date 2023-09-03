6febdac28261a90a68def876fa16a80a2c47d5b3
Pranish <pranishkafle1@gmail.com>
2023-09-03 14:12:40 +0545

    Basic Audio Helper Activity

---
+++ AiCrusade/app/src/main/java/com/example/Aicrusade/helpers/MLAudioHelperActivity.java
@@ -0,0 +1,52 @@
+package com.example.Aicrusade.helpers;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.Aicrusade.R;
+import com.example.mlseriesdemonstrator.R;
+
+public abstract class MLAudioHelperActivity extends AppCompatActivity {
+    public final static int REQUEST_RECORD_AUDIO = 2033;
+
+    protected TextView outputTextView;
+
+    protected Button startRecordingButton;
+    protected Button stopRecordingButton;
+    protected Button enterbutton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_helper);
+
+        enterbutton=findViewById(R.id.enter);
+
+        outputTextView = findViewById(R.id.textViewOutput);
+        startRecordingButton = findViewById(R.id.buttonStartRecording);
+        stopRecordingButton = findViewById(R.id.buttonStopRecording);
+
+        stopRecordingButton.setEnabled(false);
+
+        if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, REQUEST_RECORD_AUDIO);
+        }
+    }
+
+    public void onStartRecording(View view) {
+        startRecordingButton.setEnabled(false);
+        stopRecordingButton.setEnabled(true);
+    }
+
+    public void onStopRecording(View view) {
+        startRecordingButton.setEnabled(true);
+        stopRecordingButton.setEnabled(false);
+    }
+
+}
